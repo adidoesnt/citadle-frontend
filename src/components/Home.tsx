7059a7d44d1cf891b69c8a1b73fd456e099050cf
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-02-23 16:14:03 +0800

    add check for mobile devices

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,15 @@
 import Game from "./Game";
 import Layout from "./Layout";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import OnlyMobile from "./OnlyMobile";
+import { isMobile } from "react-device-detect";
+
 const queryClient = new QueryClient();
 
 function Home() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Game />
-      </Layout>
+      <Layout>{isMobile ? <Game /> : <OnlyMobile />}</Layout>
     </QueryClientProvider>
   );
 }
---
+++ src/components/OnlyMobile.tsx
@@ -0,0 +1,9 @@
+export default function OnlyMobile() {
+  return (
+    <div>
+      <p className="w-full flex text-center justify-center p-3">
+        This app is only available on mobile.
+      </p>
+    </div>
+  );
+}
